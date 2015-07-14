d8d5ab6d5f7c2b3acf2bf625c6efdcbeef447fb5
nilov <nilov@glavweb.ru>
2015-07-14 11:11:31 +0600

    Initial commit

---
+++ Form/Type/FormStaticControlRawType.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace GlavwebCoreBundle\Form\Type;
+
+use Braincrafted\Bundle\BootstrapBundle\Form\Type\FormStaticControlType;
+use Symfony\Component\Form\AbstractType;
+
+/**
+ * Class FormStaticControlRawType
+ * @package GlavwebCoreBundle\Form\Type
+ */
+class FormStaticControlRawType extends FormStaticControlType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'bs_static_raw';
+    }
+}
---
+++ GlavwebCoreBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Glavweb\ContentBlockBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class GlavwebCoreBundle extends Bundle
+{
+}
